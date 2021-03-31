883d412a2c1e19690bdfd46be5b3003b788f5c40
Kirill Smirnov <thegaldren@gmail.com>
2021-03-31 08:27:58 +0300

    Add CraftThrust.cs to craft_cargoB.prefab

---
+++ Assets/Trucker/Scripts/Control/Craft/Movement/CraftThrust.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityUtils.Variables;
+
+namespace Trucker.Control.Craft.Movement
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class CraftThrust : MonoBehaviour
+    {
+        [SerializeField] private FloatVariable thrustValue;
+
+        private Rigidbody _rb;
+        private float _craftMass;
+
+        private void Awake() => InitFields();
+
+        private void FixedUpdate() => ApplyThrust();
+
+        private void InitFields()
+        {
+            _rb = GetComponent<Rigidbody>();
+            _craftMass = _rb.mass;
+        }
+
+        private void ApplyThrust()
+        {
+            var thrustForce = Vector3.forward * thrustValue * _craftMass;
+            _rb.AddForce(thrustForce);
+        }
+    }
+}
