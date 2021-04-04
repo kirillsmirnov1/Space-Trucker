c6490da95bc9149ccffcafa36238846c851ab7cd
Kirill Smirnov <thegaldren@gmail.com>
2021-04-04 18:11:53 +0300

    ThrustSlider.cs

---
+++ Assets/Trucker/Scripts/View/Input/ThrustSlider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityUtils.Variables;
+
+namespace Trucker.View.Input
+{
+    [RequireComponent(typeof(Slider))]
+    public class ThrustSlider : MonoBehaviour
+    {
+        [SerializeField] private FloatVariable thrust;
+        private Slider _slider;
+
+        private void Awake()
+        {
+            _slider = GetComponent<Slider>();
+            _slider.onValueChanged.AddListener(SetThrust);
+            SetThrust(0);
+        }
+
+        private void OnDestroy()
+        {
+            _slider.onValueChanged.RemoveListener(SetThrust);
+        }
+
+        private void SetThrust(float sliderValue) => thrust.Value = sliderValue;
+    }
+}
