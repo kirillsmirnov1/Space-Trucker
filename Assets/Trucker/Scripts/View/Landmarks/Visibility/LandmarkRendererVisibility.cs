5de8b27fd91a1b3791eaa16b925f5f0fb62b0945
Kirill Smirnov <thegaldren@gmail.com>
2021-04-04 14:36:04 +0300

    LandmarkVisibility.cs prototype — log when parts of object became visible

---
+++ Assets/Trucker/Scripts/View/Landmarks/Visibility/LandmarkRendererVisibility.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Trucker.Scripts.View.Landmarks.Visibility
+{
+    public class LandmarkRendererVisibility : MonoBehaviour
+    {
+        // TODO notify parent 
+        private void OnBecameVisible()
+        {
+            Debug.Log($"{gameObject.name} became visible");
+        }
+
+        private void OnBecameInvisible()
+        {
+            Debug.Log($"{gameObject.name} became invisible");
+        }
+    }
+}
---
+++ Assets/Trucker/Scripts/View/Landmarks/Visibility/LandmarkVisibility.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Trucker.Scripts.View.Landmarks.Visibility
+{
+    public class LandmarkVisibility : MonoBehaviour
+    {
+        // TODO bind callbacks from LandmarkRendererVisibility 
+        // TODO display smth on canvas 
+        
+        private void Awake() => InitChildRenderers();
+
+        private void InitChildRenderers()
+        {
+            foreach (var childRenderer in GetComponentsInChildren<Renderer>())
+            {
+                childRenderer.gameObject.AddComponent<LandmarkRendererVisibility>();
+            }
+        }
+    }
+}
