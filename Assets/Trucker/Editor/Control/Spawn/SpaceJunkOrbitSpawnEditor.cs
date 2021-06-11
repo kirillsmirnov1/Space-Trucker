08b8dab75fece3e05f259c267b31641cddefd47c
Kirill Smirnov <thegaldren@gmail.com>
2021-06-11 12:46:41 +0300

    Prototype SpaceJunkOrbitSpawn.cs

---
+++ Assets/Trucker/Editor/Control/Spawn/SpaceJunkOrbitSpawnEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Trucker.Control.Spawn
+{
+    [CustomEditor(typeof(SpaceJunkOrbitSpawn))]
+    public class SpaceJunkOrbitSpawnEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            if (GUILayout.Button("Spawn"))
+            {
+                var obj = (SpaceJunkOrbitSpawn) target;
+                obj.Spawn();
+            }
+            base.OnInspectorGUI();
+        }
+    }
+}
---
+++ Assets/Trucker/Scripts/Control/Spawn/SpaceJunkOrbitSpawn.cs
@@ -0,0 +1,40 @@
+﻿using Unity.Mathematics;
+using UnityEngine;
+
+namespace Trucker.Control.Spawn
+{
+    public class SpaceJunkOrbitSpawn : MonoBehaviour
+    {
+        [SerializeField] private GameObject prefabToSpawn; // might extract factory later
+        [SerializeField] private Transform centralObject;
+        [SerializeField] private int numberOfObjects;
+        [SerializeField] private float orbitRadius;
+        [SerializeField] private float spawnCircleRadius; // might change to ellipsis later
+
+        // TODO show spawn location 
+        
+        public void Spawn()  
+        {
+            RemoveOldSpawn();
+            for (var i = 0; i < numberOfObjects; i++)
+            {
+                Instantiate(prefabToSpawn, NextPosition(), quaternion.identity, transform);
+                // TODO change size and rotation 
+            }
+        }
+
+        private void RemoveOldSpawn()
+        {
+            for (var i = transform.childCount - 1; i >= 0; i--)
+            {
+                DestroyImmediate(transform.GetChild(i).gameObject);
+            }
+        }
+
+        private Vector3 NextPosition()
+        {
+            return Vector3.zero; // TODO 
+        }
+        
+    }
+}
