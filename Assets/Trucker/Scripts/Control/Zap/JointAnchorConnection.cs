8b243258208c36c254fdfaaf71020a51478180b4
Kirill Smirnov <thegaldren@gmail.com>
2021-06-29 13:47:17 +0300

    fix spring joint affecting other body

---
+++ Assets/Trucker/Scripts/Control/Zap/JointAnchorConnection.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Trucker.Control.Zap
+{
+    public class JointAnchorConnection : MonoBehaviour
+    {
+        public Transform connectedBody;
+        public Joint joint;
+
+        private void FixedUpdate() 
+            => joint.connectedAnchor = connectedBody.position;
+    }
+}
--- Assets/Trucker/Scripts/Control/Zap/ZapCatcher.cs
+++ Assets/Trucker/Scripts/Control/Zap/ZapCatcher.cs
@@ -5,26 +5,26 @@ namespace Trucker.Control.Zap
 {
     public class ZapCatcher : MonoBehaviour
     {
-        [SerializeField] private Rigidbody rb;
         private List<ZapCatchee> catchees = new List<ZapCatchee>();
 
         public bool TryCatch(ZapCatchee zapCatchee)
         {
             if (catchees.Contains(zapCatchee)) return false;
             
-            var bodyToConnectTo = catchees.Count > 0 ? catchees[catchees.Count - 1].Rigidbody : rb;
+            var bodyToConnectTo = catchees.Count > 0 ? catchees[catchees.Count - 1].transform : transform;
             var springJoint = zapCatchee.gameObject.AddComponent<SpringJoint>();
-
-            springJoint.connectedBody = bodyToConnectTo;
 
             // TODO extract to SO 
             springJoint.autoConfigureConnectedAnchor = false;
-            springJoint.connectedAnchor = Vector3.zero;
             springJoint.minDistance = 1f;
             springJoint.maxDistance = 2f;
             springJoint.enableCollision = true;
             springJoint.spring = 3;
             springJoint.damper = 2;
+
+            var jointAnchorConnection = zapCatchee.gameObject.AddComponent<JointAnchorConnection>();
+            jointAnchorConnection.joint = springJoint;
+            jointAnchorConnection.connectedBody = bodyToConnectTo;
             
             catchees.Add(zapCatchee);
             
