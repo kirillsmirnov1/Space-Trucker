543787b64ea6548a565750dce74b23357669a677
Kirill Smirnov <thegaldren@gmail.com>
2021-07-01 14:19:59 +0300

    Extract Catching.FillerScale

--- Assets/Trucker/Scripts/Control/Zap/Catchee/States/Catching.cs
+++ Assets/Trucker/Scripts/Control/Zap/Catchee/States/Catching.cs
@@ -4,6 +4,12 @@ namespace Trucker.Control.Zap.Catchee.States
 {
     public class Catching : ZapCatcheeState
     {
+        private Vector3 FillerScale
+        {
+            get => Catchee.crosshairFiller.transform.localScale;
+            set => Catchee.crosshairFiller.transform.localScale = value;
+        }
+        
         public Catching(ZapCatchee catchee) : base(catchee) { }
 
         public override void EnterState() { }
@@ -17,10 +23,9 @@ namespace Trucker.Control.Zap.Catchee.States
 
         private void EnlargeCrosshairFiller()
         {
-            // FIXME refactor
             // FIXME speed 
-            Catchee.crosshairFiller.transform.localScale += Time.deltaTime * Vector3.one;
-            if (Catchee.crosshairFiller.transform.localScale.magnitude >= 1)
+            FillerScale += Time.deltaTime * Vector3.one;
+            if (FillerScale.magnitude >= 1)
             {
                 Catchee.SetState(new Catched(Catchee));
             }
@@ -32,9 +37,7 @@ namespace Trucker.Control.Zap.Catchee.States
         public override void OnUnreachable() 
             => Catchee.SetState(new FreeUnreachable(Catchee));
 
-        public override void ExitState()
-        {
-            Catchee.crosshairFiller.transform.localScale = Vector3.one * 0.2f; // FIXME MN 
-        }
+        public override void ExitState() 
+            => FillerScale = Vector3.one * 0.2f;
     }
 }
