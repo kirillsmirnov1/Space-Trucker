f3ac620f70a8ca25916cb56453a8abbf1b058941
Kirill Smirnov <thegaldren@gmail.com>
2021-07-01 12:38:57 +0300

    add ZapCatcheeReachable.cs status checker

---
+++ Assets/Trucker/Scripts/Control/Zap/ZapCatcheeReachable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace Trucker.Control.Zap
+{
+    public class ZapCatcheeReachable : MonoBehaviour
+    {
+        public Action<bool> onStatusChange;
+
+        private bool _reachable;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                SetStatus(true);
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                SetStatus(false);
+            }
+        }
+
+        private void SetStatus(bool reachable)
+        {
+            _reachable = reachable;
+            onStatusChange?.Invoke(_reachable);
+        }
+    }
+}
