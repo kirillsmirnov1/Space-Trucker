0454c2b3be704524ec903fce900239b1016358fd
Kirill Smirnov <thegaldren@gmail.com>
2021-07-01 12:14:06 +0300

    add crosshairs to meteors

---
+++ Assets/Trucker/Scripts/Control/Zap/ZapCatcheeCrosshair.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityUtils.Variables;
+
+namespace Trucker.Control.Zap
+{
+    public class ZapCatcheeCrosshair : MonoBehaviour
+    {
+        [SerializeField] private TransformVariable catcher;
+        [SerializeField] private Transform crosshairHolder;
+        
+        private void Update()
+        {
+            var lookPos = catcher.Value.position - crosshairHolder.position;
+            var rotation = Quaternion.LookRotation(lookPos);
+            crosshairHolder.rotation = rotation;
+        }
+    }
+}
