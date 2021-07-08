5314d94c87316add27b2060d2ac2ddf9707d82e5
Kirill Smirnov <thegaldren@gmail.com>
2021-07-08 12:16:22 +0300

    InitiatedScriptableObject.cs

---
+++ Assets/Trucker/Scripts/Model/Util/InitScriptableObjects.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Trucker.Model.Util
+{
+    public class InitScriptableObjects : MonoBehaviour
+    {
+        [SerializeField] private List<InitiatedScriptableObject> objects;
+        
+        private void Awake()
+        {
+            foreach (var obj in objects)
+            {
+                obj.Init();
+            }
+        }
+    }
+}
---
+++ Assets/Trucker/Scripts/Model/Util/InitiatedScriptableObject.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Trucker.Model.Util
+{
+    public abstract class InitiatedScriptableObject : ScriptableObject // TODO move to UU 
+    {
+        public abstract void Init();
+    }
+}
