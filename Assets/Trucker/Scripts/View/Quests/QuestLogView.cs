c5819ebc7541ae5e20bf7e95f901c7f9c42820c7
Kirill Smirnov <thegaldren@gmail.com>
2021-07-08 14:06:52 +0300

    Add open/close to QuestLogView.cs

---
+++ Assets/Trucker/Scripts/View/Quests/QuestLogView.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityUtils.Events;
+
+namespace Trucker.View.Quests
+{
+    public class QuestLogView : MonoBehaviour
+    {
+        [SerializeField] private GameEvent openEvent;
+        [SerializeField] private GameObject scroll;
+
+        private void Awake() => openEvent.RegisterAction(Show);
+
+        private void OnDestroy() => openEvent.UnregisterAction(Show);
+
+        private void Show()
+        {
+            scroll.gameObject.SetActive(true);
+            // TODO
+        }
+    }
+}
