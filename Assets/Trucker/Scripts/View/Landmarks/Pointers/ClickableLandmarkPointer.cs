37c95d7b7fd9575ed3b9f4846086b6a8fb913e80
Kirill Smirnov <thegaldren@gmail.com>
2021-07-09 14:33:14 +0300

    ClickableLandmarkPointer.cs hides when Landmark is invisible

---
+++ Assets/Trucker/Scripts/View/Landmarks/Pointers/ClickableLandmarkPointer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Trucker.View.Landmarks.Pointers
+{
+    public class ClickableLandmarkPointer : LandmarkPointer
+    {
+        [SerializeField] private GameObject button;
+        
+        public void OnClick()
+        {
+            // TODO
+        }
+
+        protected override void OnVisibilityChange(bool visible)
+        {
+            base.OnVisibilityChange(visible);
+            SetButtonVisibility();
+        }
+
+        private void SetButtonVisibility()
+        {
+            button.SetActive(LandmarkVisibleOnScreen); // TODO also use range 
+        }
+    }
+}
