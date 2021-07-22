b984850a1e0bc5c7ae4193e3c8da702de2416778
Kirill Smirnov <thegaldren@gmail.com>
2021-07-22 13:51:47 +0300

    Show first lines of dialogue

--- Assets/Trucker/Scripts/View/Dialogue/DialogueView.cs
+++ Assets/Trucker/Scripts/View/Dialogue/DialogueView.cs
@@ -1,19 +1,24 @@
-﻿using TMPro;
+﻿using System.Linq;
+using TMPro;
 using Trucker.Model.NPC;
+using Trucker.View.Util;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityUtils;
 
 namespace Trucker.View.Dialogue
 {
-    public class DialogueView : MonoBehaviour
+    public class DialogueView : ListView<string>
     {
         [SerializeField] private GameObject fade;
         [SerializeField] private TextMeshProUGUI npcNameText;
         [SerializeField] private Image npcPortrait;
 
-        private void OnValidate() 
-            => this.CheckNullFields();
+        protected override void OnValidate()
+        {
+            base.OnValidate();
+            this.CheckNullFields();
+        }
 
         private void Awake() 
             => DialogueProvider.OnDialogueInitiated += ShowDialogue;
@@ -27,7 +32,7 @@ namespace Trucker.View.Dialogue
             npcPortrait.sprite = npcPicture;
             fade.gameObject.SetActive(true);
             LayoutRebuilder.ForceRebuildLayoutImmediate(npcNameText.rectTransform);
-            // TODO use dialogue 
+            SetEntries(dialogues.Select(d => d.GetLines()[0]).ToList()); // IMPR this is the best way???
         }
     }
 }
---
+++ Assets/Trucker/Scripts/View/Dialogue/DialogueViewEntry.cs
@@ -0,0 +1,18 @@
+﻿using TMPro;
+using Trucker.View.Util;
+using UnityEngine;
+
+namespace Trucker.View.Dialogue
+{
+    public class DialogueViewEntry : ListViewEntry<string>
+    {
+        [SerializeField] private TextMeshProUGUI text;
+        // TODO button 
+        // TODO id 
+        
+        public override void Fill(string data)
+        {
+            text.text = data;
+        }
+    }
+}
