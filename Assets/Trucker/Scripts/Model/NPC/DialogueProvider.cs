c4cdcdad8830b5f0958df60c19feb0ae79488c78
Kirill Smirnov <thegaldren@gmail.com>
2021-07-22 12:45:02 +0300

    Add DialogueView.cs to Canvas_Dialogue.prefab

--- Assets/Trucker/Scripts/Model/NPC/DialogueProvider.cs
+++ Assets/Trucker/Scripts/Model/NPC/DialogueProvider.cs
@@ -1,13 +1,20 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using UnityEngine;
 
 namespace Trucker.Model.NPC
 {
     public class DialogueProvider : MonoBehaviour
     {
+        public static event Action<IDialogue[]> OnDialogueInitiated; 
+        
+        // TODO sprite and name
         [SerializeField] private ScriptableObject[] dialogueOptions; // Unity still doesnt serialize interfaces, so here we go 
 
         private IDialogue[] GetDialogues() 
             => dialogueOptions.Select(x => (IDialogue) x).ToArray();
+
+        public void InitiateDialogue()
+            => OnDialogueInitiated?.Invoke(GetDialogues());
     }
 }
---
+++ Assets/Trucker/Scripts/View/Dialogue/DialogueView.cs
@@ -0,0 +1,22 @@
+﻿using Trucker.Model.NPC;
+using UnityEngine;
+
+namespace Trucker.View.Dialogue
+{
+    public class DialogueView : MonoBehaviour
+    {
+        [SerializeField] private GameObject fade;
+        
+        private void Awake() 
+            => DialogueProvider.OnDialogueInitiated += ShowDialogue;
+
+        private void OnDestroy() 
+            => DialogueProvider.OnDialogueInitiated -= ShowDialogue;
+
+        private void ShowDialogue(IDialogue[] dialogueOptions)
+        {
+            fade.gameObject.SetActive(true);
+            // TODO use dialogue 
+        }
+    }
+}
