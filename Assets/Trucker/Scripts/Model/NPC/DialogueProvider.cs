e9a8e5f3f23822ed7a937bba46473cab44850d71
Kirill Smirnov <thegaldren@gmail.com>
2021-07-22 12:52:42 +0300

    Pass name and sprite to dialogue

--- Assets/Trucker/Scripts/Model/NPC/DialogueProvider.cs
+++ Assets/Trucker/Scripts/Model/NPC/DialogueProvider.cs
@@ -1,20 +1,25 @@
 ﻿using System;
 using System.Linq;
 using UnityEngine;
+using UnityUtils;
 
 namespace Trucker.Model.NPC
 {
     public class DialogueProvider : MonoBehaviour
     {
-        public static event Action<IDialogue[]> OnDialogueInitiated; 
-        
-        // TODO sprite and name
+        public static event Action<string, Sprite, IDialogue[]> OnDialogueInitiated;
+
+        [SerializeField] private string npcName;
+        [SerializeField] private Sprite npcPicture;
         [SerializeField] private ScriptableObject[] dialogueOptions; // Unity still doesnt serialize interfaces, so here we go 
+
+        private void OnValidate() 
+            => this.CheckNullFields();
 
         private IDialogue[] GetDialogues() 
             => dialogueOptions.Select(x => (IDialogue) x).ToArray();
 
         public void InitiateDialogue()
-            => OnDialogueInitiated?.Invoke(GetDialogues());
+            => OnDialogueInitiated?.Invoke(npcName, npcPicture, GetDialogues());
     }
 }
--- Assets/Trucker/Scripts/View/Dialogue/DialogueView.cs
+++ Assets/Trucker/Scripts/View/Dialogue/DialogueView.cs
@@ -1,20 +1,30 @@
-﻿using Trucker.Model.NPC;
+﻿using TMPro;
+using Trucker.Model.NPC;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityUtils;
 
 namespace Trucker.View.Dialogue
 {
     public class DialogueView : MonoBehaviour
     {
         [SerializeField] private GameObject fade;
-        
+        [SerializeField] private TextMeshProUGUI npcNameText;
+        [SerializeField] private Image npcPortrait;
+
+        private void OnValidate() 
+            => this.CheckNullFields();
+
         private void Awake() 
             => DialogueProvider.OnDialogueInitiated += ShowDialogue;
 
         private void OnDestroy() 
             => DialogueProvider.OnDialogueInitiated -= ShowDialogue;
 
-        private void ShowDialogue(IDialogue[] dialogueOptions)
+        private void ShowDialogue(string npcName, Sprite npcPicture, IDialogue[] dialogues)
         {
+            npcNameText.text = npcName;
+            npcPortrait.sprite = npcPicture;
             fade.gameObject.SetActive(true);
             // TODO use dialogue 
         }
