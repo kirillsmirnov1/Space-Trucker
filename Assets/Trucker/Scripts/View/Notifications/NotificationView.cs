a38d3b8c0853097666aae41becd36c7e05a6abcd
Kirill Smirnov <thegaldren@gmail.com>
2021-07-30 13:08:36 +0300

    NotificationView.cs display coroutine

--- Assets/Trucker/Scripts/View/Notifications/NotificationView.cs
+++ Assets/Trucker/Scripts/View/Notifications/NotificationView.cs
@@ -1,4 +1,5 @@
-﻿using TMPro;
+﻿using System.Collections;
+using TMPro;
 using UnityEngine;
 
 namespace Trucker.View.Notifications
@@ -6,5 +7,44 @@ namespace Trucker.View.Notifications
     public class NotificationView : MonoBehaviour
     {
         [SerializeField] private TextMeshProUGUI text;
+
+        private const float ShowDuration = 5f; // IMPR extract settings 
+        private const float FadeDuration = 1f;
+        private const int FadeSteps = 20;
+
+        public void Display(string str, bool strikethrough)
+        {
+            StopAllCoroutines();
+            SetText(str, strikethrough);
+            SetObject();
+            StartCoroutine(DisplayCoroutine());
+        }
+
+        private void SetText(string str, bool strikethrough)
+        {
+            text.text = str;
+            text.fontStyle = strikethrough ? FontStyles.Bold | FontStyles.Strikethrough : FontStyles.Bold;
+            text.color = Color.white;
+        }
+
+        private void SetObject()
+        {
+            transform.SetAsLastSibling();
+            gameObject.SetActive(true);
+        }
+
+        private IEnumerator DisplayCoroutine()
+        {
+            yield return new WaitForSeconds(ShowDuration);
+
+            for (float i = FadeSteps; i >= 0; i--)
+            {
+                yield return new WaitForSeconds(FadeDuration / FadeSteps);
+                var alpha = i / FadeSteps;
+                text.color = new Color(1, 1, 1, alpha);
+            }
+            
+            gameObject.SetActive(false);
+        }
     }
 }
