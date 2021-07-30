354e618fb5f1a0d2c2f9e2ee857f65417ecc0686
Kirill Smirnov <thegaldren@gmail.com>
2021-07-30 13:08:57 +0300

    NotificationsDisplay.cs shows quest notifications

--- Assets/Trucker/Scripts/View/Notifications/NotificationsDisplay.cs
+++ Assets/Trucker/Scripts/View/Notifications/NotificationsDisplay.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Trucker.Model.Questing.Quests;
+using UnityEngine;
 
 namespace Trucker.View.Notifications
 {
@@ -6,9 +7,35 @@ namespace Trucker.View.Notifications
     {
         [SerializeField] private NotificationView[] notifications;
 
-        private void OnValidate()
+        private int _nextNotificationIndex;
+        
+        private void OnValidate() 
+            => notifications = GetComponentsInChildren<NotificationView>(true);
+
+        private void Awake()
         {
-            notifications = GetComponentsInChildren<NotificationView>(true);
+            Quest.OnQuestTaken += OnQuestTaken;
+            Quest.OnQuestFinished += OnQuestFinished;
+            // TODO goals 
+            // TODO object detach 
+        }
+
+        private void OnDestroy()
+        {
+            Quest.OnQuestTaken -= OnQuestTaken;
+            Quest.OnQuestFinished -= OnQuestFinished;
+        }
+
+        private void OnQuestTaken(string title) 
+            => Notify($"{title} starts", false);
+
+        private void OnQuestFinished(string title) 
+            => Notify($"{title} finished", false);
+
+        private void Notify(string str, bool strikethrough)
+        {
+            notifications[_nextNotificationIndex].Display(str, strikethrough);
+            _nextNotificationIndex = (_nextNotificationIndex + 1) % notifications.Length;
         }
     }
 }
