0ca2f54aa7b45e035c0184e95c686d559168d6a1
Kirill Smirnov <thegaldren@gmail.com>
2021-08-06 17:36:20 +0300

    Add CharactersData.asset

---
+++ Assets/Trucker/Scripts/Model/NPC/CharacterName.cs
@@ -0,0 +1,18 @@
+﻿namespace Trucker.Model.NPC
+{
+    public enum CharacterName
+    {
+        Ackerley,
+        Berthilda,
+        Child0,
+        Child1,
+        Child2,
+        Child3,
+        Claiborne,
+        Darnell,
+        Eardley,
+        Fielding,
+        Godwin,
+        Merrill,
+    }
+}
---
+++ Assets/Trucker/Scripts/Model/NPC/CharactersData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using UnityUtils.Attributes;
+
+namespace Trucker.Model.NPC
+{
+    [CreateAssetMenu(fileName = "CharactersData", menuName = "Data/Characters Data", order = 0)]
+    public class CharactersData : ScriptableObject
+    {
+        [NamedArray(typeof(CharacterData))]
+        [SerializeField]
+        private CharacterData[] characters;
+
+        private void OnValidate() => ParseStringNames();
+
+        private void ParseStringNames()
+        {
+            for (int i = 0; i < characters.Length; i++)
+            {
+                characters[i].nameText = characters[i].name.ToString();
+            }
+        }
+
+        public CharacterData GetCharacterData(CharacterName charName) // IMPR use Dict
+            => characters.First(ch => ch.name == charName);
+    }
+
+    [Serializable]
+    public struct CharacterData
+    {
+        public CharacterName name;
+        public string nameText;
+        public Sprite portrait;
+        public string description;
+    }
+}
