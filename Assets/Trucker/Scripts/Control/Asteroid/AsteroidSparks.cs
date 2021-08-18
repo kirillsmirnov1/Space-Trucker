7c96a2934057bbec83ef4f4712a3dbd75f33e27b
Kirill Smirnov <thegaldren@gmail.com>
2021-08-18 11:26:23 +0300

    AsteroidSparks.cs are turning on and off

--- Assets/Trucker/Scripts/Control/Asteroid/AsteroidSparks.cs
+++ Assets/Trucker/Scripts/Control/Asteroid/AsteroidSparks.cs
@@ -1,11 +1,41 @@
+using System.Collections;
 using UnityEngine;
+using UnityUtils;
+using UnityUtils.Variables;
+using Random = System.Random;
 
 namespace Trucker.Control.Asteroid
 {
     public class AsteroidSparks : MonoBehaviour
     {
-        // TODO at spawn start timer
-        // TODO sparks will turn off after some time
+        [SerializeField] private ParticleSystem sparkParticles;
+        [SerializeField] private Vector2Variable sparkDelay;
+        [SerializeField] private Vector2Variable sparkDuration;
+        
+        private static readonly Random Random = new Random();
+        private bool _sparkling;
+        
+        private void Awake()
+        {
+            StartCoroutine(DelaySparksTurningOn());
+        }
+
+        private IEnumerator DelaySparksTurningOn()
+        {
+            yield return new WaitForSeconds(Random.NextFloat(sparkDelay.Value.x, sparkDelay.Value.y)); // IMPR x/y property to v2var
+            sparkParticles.Play();
+            _sparkling = true;
+            yield return DelaySparksTurningOff();
+        }
+
+        private IEnumerator DelaySparksTurningOff()
+        {
+            yield return new WaitForSeconds(Random.NextFloat(sparkDuration.Value.x, sparkDuration.Value.y));
+            sparkParticles.Stop();
+            _sparkling = false;
+            yield return DelaySparksTurningOn();
+        }
+        
         // TODO catchStart locks sparks
         // TODO catchRelease releases 
         // TODO change type to AsteroidWith Sparks 
