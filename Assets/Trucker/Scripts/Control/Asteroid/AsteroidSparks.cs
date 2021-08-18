d7b72cc271a9f6734da7288465e1f21d52652a81
Kirill Smirnov <thegaldren@gmail.com>
2021-08-18 11:12:44 +0300

    Add stub AsteroidSparks.cs

---
+++ Assets/Trucker/Scripts/Control/Asteroid/AsteroidSparks.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Trucker.Control.Asteroid
+{
+    public class AsteroidSparks : MonoBehaviour
+    {
+        // TODO at spawn start timer
+        // TODO sparks will turn off after some time
+        // TODO catchStart locks sparks
+        // TODO catchRelease releases 
+        // TODO change type to AsteroidWith Sparks 
+    }
+}
