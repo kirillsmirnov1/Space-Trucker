892563a3971482fd60a6467bbb16f8d1b911974d
Kirill Smirnov <thegaldren@gmail.com>
2021-08-19 13:17:35 +0300

    SetTransformPosition.cs

---
+++ Assets/Trucker/Scripts/Model/Questing/Steps/Operations/SetTransformPosition.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityUtils.Variables;
+
+namespace Trucker.Model.Questing.Steps.Operations
+{
+    [CreateAssetMenu(menuName = "Quests/Operations/SetTransformPosition", fileName = "SetTransformPosition", order = 0)]
+    public class SetTransformPosition : Operation
+    {
+        [SerializeField] private TransformVariable subject;
+        [SerializeField] private Vector3 desiredPosition;
+        
+        public override void Start()
+        {
+            subject.Value.position = desiredPosition;
+            onCompleted?.Invoke();
+        }
+    }
+}
