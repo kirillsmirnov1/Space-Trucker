f4ef16719b81fa59e1e757f8862d2d47ec025e39
Kirill Smirnov <thegaldren@gmail.com>
2021-08-19 14:17:47 +0300

    ZapLevel.cs + save it

---
+++ Assets/Trucker/Scripts/Model/Zap/ZapLevel.cs
@@ -0,0 +1,9 @@
+﻿namespace Trucker.Model.Zap
+{
+    public enum ZapLevel
+    {
+        Disabled = 0,
+        Base = 5,
+        Plus = 15,
+    }
+}
---
+++ Assets/Trucker/Scripts/Model/Zap/ZapLevelVariable.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityUtils.Variables;
+
+namespace Trucker.Model.Zap
+{
+    [CreateAssetMenu(menuName = "Variables/ZapLevelVariable", fileName = "ZapLevelVariable", order = 0)]
+    public class ZapLevelVariable : XVariable<ZapLevel>
+    {
+        
+    }
+}
