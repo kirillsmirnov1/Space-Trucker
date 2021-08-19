bebc06456fd6ec77064612955d08d770a4c466a1
Kirill Smirnov <thegaldren@gmail.com>
2021-08-19 13:56:52 +0300

    SetBoolVariable.cs

---
+++ Assets/Trucker/Scripts/Model/Questing/Steps/Operations/SetBoolVariable.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityUtils.Variables;
+
+namespace Trucker.Model.Questing.Steps.Operations
+{
+    [CreateAssetMenu(menuName = "Quests/Operations/SetBoolVariable", fileName = "SetBoolVariable", order = 0)]
+    public class SetBoolVariable : Operation
+    {
+        [SerializeField] private BoolVariable variable;
+        [SerializeField] private bool value;
+        
+        public override void Start()
+        {
+            variable.Value = value;
+            onCompleted?.Invoke();
+        }
+    }
+}
