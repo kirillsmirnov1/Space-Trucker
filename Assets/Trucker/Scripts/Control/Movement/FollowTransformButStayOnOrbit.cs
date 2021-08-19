e71d8d5f99193c2e9ffdb8db9a9e6a14e25f6b91
Kirill Smirnov <thegaldren@gmail.com>
2021-08-19 14:11:56 +0300

    InvokeGameEvent.cs and Op_ResetOrbitSpawn.asset for _Quest_01_Get_To_Dump.asset

--- Assets/Trucker/Scripts/Control/Movement/FollowTransformButStayOnOrbit.cs
+++ Assets/Trucker/Scripts/Control/Movement/FollowTransformButStayOnOrbit.cs
@@ -10,9 +10,13 @@ namespace Trucker.Control.Movement
         
         private void FixedUpdate()
         {
+            UpdatePosition();
+        }
+
+        public void UpdatePosition()
+        {
             // Q is on (0,0,0), so there is no need to subtract it's position, for now
             transform.position = transformToFollow.Value.position.normalized * orbitRadius;
         }
-        
     }
 }
---
+++ Assets/Trucker/Scripts/Model/Questing/Steps/Operations/InvokeGameEvent.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityUtils.Events;
+
+namespace Trucker.Model.Questing.Steps.Operations
+{
+    [CreateAssetMenu(menuName = "Quests/Operations/InvokeGameEvent", fileName = "InvokeGameEvent", order = 0)]
+    public class InvokeGameEvent : Operation
+    {
+        [SerializeField] private GameEvent gameEvent;
+        
+        public override void Start()
+        {
+            gameEvent.Raise();
+            onCompleted?.Invoke();
+        }
+    }
+}
