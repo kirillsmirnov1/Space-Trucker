06dc973549be252b9f6a07cdf2c3d359c6485226
Kirill Smirnov <thegaldren@gmail.com>
2021-08-24 13:58:48 +0300

    MovementTutorial.cs tilt step

---
+++ Assets/Trucker/Scripts/View/Tutorials/MovementTutorial.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+namespace Trucker.View.Tutorials
+{
+    public class MovementTutorial : MonoBehaviour
+    {
+        [Header("Components")]
+        [SerializeField] private Transform phone;
+        [SerializeField] private TextMeshProUGUI prompt;
+
+        [Header("Data")]
+        [SerializeField] private float maxAngle = 45f;
+        [SerializeField] private float iterationDuration = 2f;
+        [SerializeField] private int iterationSteps = 120;
+
+        public void StartTutorial()
+        {
+            gameObject.SetActive(true);
+            StopAllCoroutines();
+            StartCoroutine(Tutorial());
+        }
+
+        private IEnumerator Tutorial()
+        {
+            // TODO Idle
+            yield return TiltTutorial();
+            // TODO rotation 
+            // TODO thrust 
+            // TODO callback 
+            gameObject.SetActive(false);
+        }
+        
+        private IEnumerator TiltTutorial()
+        {
+            prompt.text = "tilt phone to tilt ship";
+            yield return RotateTransform(phone, Vector3.right, 2);
+        }
+
+        private IEnumerator RotateTransform(Transform subject, Vector3 axis, int times)
+        {
+            var wait = new WaitForSeconds(iterationDuration/iterationSteps);
+            var totalAngleDistance = maxAngle * 4;
+            var anglePerStep = totalAngleDistance / iterationSteps;
+            
+            for (int i = 0; i < times; i++)
+            {
+                var stepChangeDir = iterationSteps / 4;
+                for (int step = 0; step < iterationSteps; step++)
+                {
+                    if (step == stepChangeDir)
+                    {
+                        anglePerStep = -anglePerStep;
+                        stepChangeDir *= 3;
+                    }
+
+                    subject.Rotate(axis, anglePerStep);
+                    
+                    yield return wait;
+                }
+            }
+        }
+    }
+}
