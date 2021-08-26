d0e0ed6559a46c2c5e978c43536ee6b0acabcd20
Kirill Smirnov <thegaldren@gmail.com>
2021-08-26 14:14:37 +0300

    StepEditor.cs with start button

---
+++ Assets/Trucker/Editor/Model/Questing/Quests/Steps/StepEditor.cs
@@ -0,0 +1,19 @@
+﻿using Trucker.Model.Questing.Steps;
+using UnityEditor;
+using UnityEngine;
+
+namespace Trucker.Model.Questing.Quests.Steps
+{
+    [CustomEditor(typeof(Step), true)]
+    public class StepEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            if (GUILayout.Button("Start"))
+            {
+                ((Step)target).Start();
+            }
+        }
+    }
+}
