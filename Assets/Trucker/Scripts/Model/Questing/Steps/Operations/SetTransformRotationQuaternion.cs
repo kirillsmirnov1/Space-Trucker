d96cd4f16110cc1332ceb2e8144ed33513b2456e
Kirill Smirnov <thegaldren@gmail.com>
2021-08-26 13:07:06 +0300

    SetTransformRotationQuaternion.cs

---
+++ Assets/Trucker/Scripts/Model/Questing/Steps/Operations/SetTransformRotationQuaternion.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityUtils.Variables;
+
+namespace Trucker.Model.Questing.Steps.Operations
+{
+    [CreateAssetMenu(menuName = "Quests/Operations/SetTransformRotationQuaternion", fileName = "SetTransformRotationQuaternion", order = 0)]
+    public class SetTransformRotationQuaternion : Operation
+    {
+        [SerializeField] private TransformVariable subject;
+        [SerializeField] private Quaternion desiredRotation;
+
+        public override void Start()
+        {
+            subject.Value.rotation = desiredRotation;
+            onCompleted?.Invoke();
+        }
+    }
+}
