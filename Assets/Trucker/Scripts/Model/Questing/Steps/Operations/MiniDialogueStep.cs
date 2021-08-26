a7563aaf998a8c79a10958a694f1f06ed312f9ab
Kirill Smirnov <thegaldren@gmail.com>
2021-08-26 13:51:37 +0300

    MiniDialogueStep.cs

---
+++ Assets/Trucker/Scripts/Model/Questing/Steps/Operations/MiniDialogueStep.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Trucker.Model.NPC;
+using UnityEngine;
+
+namespace Trucker.Model.Questing.Steps.Operations
+{
+    [CreateAssetMenu(menuName = "Quests/Operations/MiniDialogueStep", fileName = "MiniDialogueStep", order = 0)]
+    public class MiniDialogueStep : Operation
+    {
+        public static event Action<IDialogue, Action> OnMiniDialogueRequest; 
+        
+        [SerializeField] private Dialogue dialogueToPlay;
+        
+        public override void Start()
+        {
+            OnMiniDialogueRequest?.Invoke(dialogueToPlay, onCompleted);
+        }
+    }
+}
