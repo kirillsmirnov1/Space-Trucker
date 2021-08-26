42fca87728c4fa1af94b351d3c42dcdc29dc7e1b
Kirill Smirnov <thegaldren@gmail.com>
2021-08-26 13:51:50 +0300

    Stub MiniDialogueView.cs

---
+++ Assets/Trucker/Scripts/View/Dialogue/MiniDialogueView.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TMPro;
+using Trucker.Model.NPC;
+using Trucker.Model.Questing.Steps.Operations;
+using UnityEngine;
+
+namespace Trucker.View.Dialogue
+{
+    public class MiniDialogueView : MonoBehaviour
+    {
+        [SerializeField] private CharacterPortrait characterPortrait;
+        [SerializeField] private TextMeshProUGUI dialogueText;
+        [SerializeField] private CharactersData charactersData;
+        
+        private void Awake() 
+            => MiniDialogueStep.OnMiniDialogueRequest += ShowMiniDialogue;
+
+        private void OnDestroy() 
+            => MiniDialogueStep.OnMiniDialogueRequest -= ShowMiniDialogue;
+
+        private void ShowMiniDialogue(IDialogue dialogue, Action finishCallback)
+        {
+            // TODO iterate over lines 
+            // TODO invoke finish callback 
+        }
+    }
+}
