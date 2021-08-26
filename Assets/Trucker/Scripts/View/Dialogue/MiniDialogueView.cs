1134b175f285664abd86cb063ad2f3c42428c87c
Kirill Smirnov <thegaldren@gmail.com>
2021-08-26 14:07:52 +0300

    Base MiniDialogueView.cs

--- Assets/Trucker/Scripts/View/Dialogue/MiniDialogueView.cs
+++ Assets/Trucker/Scripts/View/Dialogue/MiniDialogueView.cs
@@ -3,17 +3,23 @@ using TMPro;
 using Trucker.Model.NPC;
 using Trucker.Model.Questing.Steps.Operations;
 using UnityEngine;
+using UnityUtils.Variables;
 
 namespace Trucker.View.Dialogue
 {
     public class MiniDialogueView : MonoBehaviour
     {
+        [SerializeField] private GameObject dialogueWrap;
         [SerializeField] private CharacterPortrait characterPortrait;
         [SerializeField] private TextMeshProUGUI dialogueText;
         [SerializeField] private CharactersData charactersData;
+        [SerializeField] private FloatVariable secondsPerLine;
         
-        private IDialogue _dialogue;
+        private DialogueLine[] _lines;
         private Action _finishCallback;
+        private int _lineIndex;
+        private float _lineTimeLeft;
+        private Action _onUpdate;
 
         private void Awake() 
             => MiniDialogueStep.OnMiniDialogueRequest += ShowMiniDialogue;
@@ -21,13 +27,55 @@ namespace Trucker.View.Dialogue
         private void OnDestroy() 
             => MiniDialogueStep.OnMiniDialogueRequest -= ShowMiniDialogue;
 
+        private void Update() 
+            => _onUpdate?.Invoke();
+
         private void ShowMiniDialogue(IDialogue dialogue, Action finishCallback)
         {
-            _dialogue = dialogue;
+            _lines = dialogue.Lines;
             _finishCallback = finishCallback;
-        
-            // TODO iterate over lines 
-            // TODO invoke finish callback 
+            _lineIndex = -1;
+
+            IterateLines();
+            dialogueWrap.SetActive(true);
+        }
+
+        private void IterateLines() // TODO call on click 
+        {
+            _lineIndex++;
+            if (_lineIndex < _lines.Length)
+            {
+                SetNextLine();
+                ResetLineTimer();
+            }
+            else
+            {
+                dialogueWrap.SetActive(false);
+                _finishCallback?.Invoke();
+            }
+        }
+
+        private void SetNextLine()
+        {
+            characterPortrait.SetPortrait(charactersData.GetCharacterData(_lines[_lineIndex].character).portrait);
+            dialogueText.text = _lines[_lineIndex].line;
+        }
+
+        private void ResetLineTimer()
+        {
+            _lineTimeLeft = secondsPerLine;
+            _onUpdate = TickDialogueTime;
+        }
+
+        private void TickDialogueTime()
+        {
+            _lineTimeLeft -= Time.deltaTime;
+            
+            if (_lineTimeLeft <= 0f)
+            {
+                _onUpdate = null;
+                IterateLines();
+            }
         }
     }
 }
