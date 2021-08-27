db4eb3519999831fe2c1212632c2c17a1e018ca7
Kirill Smirnov <thegaldren@gmail.com>
2021-08-27 15:36:42 +0300

    Quest 1 starts quest 2

---
+++ Assets/Trucker/Scripts/Model/Questing/Steps/Operations/TakeQuestStep.cs
@@ -0,0 +1,17 @@
+﻿using Trucker.Model.Questing.Quests;
+using UnityEngine;
+
+namespace Trucker.Model.Questing.Steps.Operations
+{
+    [CreateAssetMenu(menuName = "Quests/Operations/TakeQuestStep", fileName = "TakeQuestStep", order = 0)]
+    public class TakeQuestStep : Operation
+    {
+        [SerializeField] private Quest quest;
+        
+        public override void Start()
+        {
+            quest.Take();
+            onCompleted?.Invoke();
+        }
+    }
+}
