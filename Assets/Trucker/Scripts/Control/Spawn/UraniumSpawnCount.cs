6720e7d183f5bef5717f8612ce8f024d236ef967
Kirill Smirnov <thegaldren@gmail.com>
2021-08-30 15:06:25 +0300

    UraniumSpawnCount.cs

---
+++ Assets/Trucker/Scripts/Control/Spawn/UraniumSpawnCount.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityUtils.Events;
+using UnityUtils.Variables;
+
+namespace Trucker.Control.Spawn
+{
+    [CreateAssetMenu(menuName = "Logic/UraniumSpawnCount", fileName = "UraniumSpawnCount", order = 0)]
+    public class UraniumSpawnCount : ScriptableObject
+    {
+        [SerializeField] private GameEvent respawnUraniumOnOrbit;
+        [SerializeField] private GameEvent respawnUraniumInDump;
+
+        [SerializeField] private IntVariable baseUraniumCount;
+        [SerializeField] private IntVariable orbitUraniumCount;
+        [SerializeField] private IntVariable dumpUraniumCount;
+
+
+        public void SetOrbitCount(int value)
+        {
+            orbitUraniumCount.Value = value;
+            dumpUraniumCount.Value = baseUraniumCount.Value - value;
+            respawnUraniumOnOrbit.Raise();
+            respawnUraniumInDump.Raise();
+        }
+    }
+}
---
+++ Assets/Trucker/Scripts/Model/Questing/Steps/Operations/SetUraniumSpawnCount.cs
@@ -0,0 +1,18 @@
+﻿using Trucker.Control.Spawn;
+using UnityEngine;
+
+namespace Trucker.Model.Questing.Steps.Operations
+{
+    [CreateAssetMenu(menuName = "Quests/Operations/SetUraniumSpawnCount", fileName = "SetUraniumSpawnCount", order = 0)]
+    public class SetUraniumSpawnCount : Operation
+    {
+        [SerializeField] private UraniumSpawnCount uraniumSpawnCount;
+        [SerializeField] private int newOrbitCount;
+        
+        public override void Start()
+        {
+            uraniumSpawnCount.SetOrbitCount(newOrbitCount);
+            onCompleted?.Invoke();
+        }
+    }
+}
