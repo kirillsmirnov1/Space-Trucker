19e4d189c82aadcf918c0d4fc49c5b68acfb0ab0
Kirill Smirnov <thegaldren@gmail.com>
2021-08-30 13:31:11 +0300

    Setting uranium count

---
+++ Assets/Trucker/Scripts/Model/Questing/Steps/Operations/SetIntVariable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Trucker.Model.Questing.Steps.Operations
+{
+    [CreateAssetMenu(menuName = "Quests/Operations/SetIntVariable", fileName = "SetBoolVariable", order = 0)]
+    public class SetIntVariable : SetVariableOperation<int>
+    {
+        
+    }
+}
