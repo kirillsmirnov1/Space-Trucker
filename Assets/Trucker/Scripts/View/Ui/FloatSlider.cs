c3b34fd48fbdce5ea690691909b69103cc17d81d
Kirill Smirnov <thegaldren@gmail.com>
2021-09-01 14:05:42 +0300

    Music volume slider

---
+++ Assets/Trucker/Scripts/View/Ui/FloatSlider.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityUtils.Variables;
+
+namespace Trucker.View.Ui
+{
+    public class FloatSlider : MonoBehaviour
+    {
+        [SerializeField] private FloatVariable variable;
+        [SerializeField] private Slider slider;
+
+        private void OnEnable()
+        {
+            slider.value = variable;
+        }
+
+        public void SetValue(float value)
+        {
+            variable.Value = value;
+        }
+    }
+}
