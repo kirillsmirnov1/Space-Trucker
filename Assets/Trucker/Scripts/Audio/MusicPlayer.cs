0707928a41ba83fab4c07b0449f8447ec385f1aa
Kirill Smirnov <thegaldren@gmail.com>
2021-09-01 12:24:26 +0300

    Add some stub music player

---
+++ Assets/Trucker/Scripts/Audio/MusicPlayer.cs
@@ -0,0 +1,51 @@
+﻿using Trucker.Model.Landmarks;
+using Trucker.View.Dialogue;
+using UnityEngine;
+using UnityUtils;
+
+namespace Trucker.Audio
+{
+    public class MusicPlayer : MonoBehaviour
+    {
+        [Header("Components")]
+        [SerializeField] private AudioSource playerMusic;
+        [SerializeField] private AudioSource locationMusic;
+        
+        [Header("Events")]
+        [SerializeField] private LandmarkTypeEvent onLandmarkInteraction;
+
+        private void OnValidate() => this.CheckNullFields();
+
+        private void Awake()
+        {
+            DialogueView.OnClose += OnLandmarkInteractionEnd;
+            onLandmarkInteraction.RegisterAction(OnLandmarkInteractionStart);
+        }
+
+        private void OnDestroy()
+        {
+            DialogueView.OnClose -= OnLandmarkInteractionEnd;
+            onLandmarkInteraction.UnregisterAction(OnLandmarkInteractionStart);
+        }
+
+        private void OnLandmarkInteractionStart(LandmarkType landmarkType)
+        {
+            // TODO set locationMusicFile
+            CrossFade(playerMusic, locationMusic);
+        }
+
+        private void OnLandmarkInteractionEnd()
+        {
+            CrossFade(locationMusic, playerMusic);
+        }
+
+        private void CrossFade(AudioSource from, AudioSource to)
+        {
+            // TODO use some coroutine for actual crossfade 
+            from.Stop();
+            to.Play();
+        }
+
+        // TODO OnPlayerMusicChange
+    }
+}
