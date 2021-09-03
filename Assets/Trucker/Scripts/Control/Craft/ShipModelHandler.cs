f136f187592ba05447be59ed2527d78feab3ee44
Kirill Smirnov <thegaldren@gmail.com>
2021-09-03 13:34:40 +0300

    ShipModelHandler.cs changes mesh

---
+++ Assets/Trucker/Scripts/Control/Craft/ShipModelHandler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using OneLine;
+using Trucker.Model.Craft;
+using UnityEngine;
+using UnityUtils.Attributes;
+
+namespace Trucker.Control.Craft
+{
+    public class ShipModelHandler : MonoBehaviour
+    {
+        [SerializeField] private ShipModelVariable shipModelVariable;
+        [NamedArray(typeof(ShipModel))] [OneLine]
+        [SerializeField] private ShipModelData[] modelData;
+        [SerializeField] private GameObject lastMesh;
+
+        private void Awake()
+        {
+            SetShipModel(shipModelVariable);
+            shipModelVariable.OnChange += SetShipModel;
+        }
+
+        private void OnDestroy()
+        {
+            shipModelVariable.OnChange -= SetShipModel;
+        }
+
+        private void SetShipModel(ShipModel newShipModel)
+        {
+            var newModelData = Data(newShipModel);
+            SetMesh(newModelData.mesh);
+        }
+
+        private void SetMesh(GameObject newMesh)
+        {
+            newMesh.gameObject.SetActive(true);
+            lastMesh.SetActive(false);
+            lastMesh = newMesh;
+        }
+
+        private ShipModelData Data(ShipModel model)
+            => modelData[(int) model];
+    }
+
+    [Serializable]
+    public struct ShipModelData
+    {
+        public GameObject mesh;
+    }
+}
