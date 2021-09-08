df3d6230a843274129646412254a5b8d5ae2d977
Kirill Smirnov <thegaldren@gmail.com>
2021-09-08 16:04:26 +0300

    Add UraniumHitWatcher.cs

---
+++ Assets/Trucker/Scripts/Control/Entities/UraniumHitWatcher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Trucker.Control.Entities
+{
+    [RequireComponent(typeof(Collider))]
+    public class UraniumHitWatcher : MonoBehaviour
+    {
+        public static event Action OnUraniumHit;
+        private void OnCollisionEnter(Collision other)
+        {
+            if (other.collider.CompareTag("Uranium"))
+            {
+                OnUraniumHit?.Invoke();
+            }
+        }
+    }
+}
