4db5f8f59a217b15650ae78bc2a272eefc8ebc1a
Kirill Smirnov <thegaldren@gmail.com>
2021-09-09 11:29:53 +0300

    BoolGoal.cs

---
+++ Assets/Trucker/Scripts/Model/Questing/Steps/Goals/BoolGoal.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityUtils.Variables;
+
+namespace Trucker.Model.Questing.Steps.Goals
+{
+    [CreateAssetMenu(menuName = "Quests/Goals/BoolGoal", fileName = "BoolGoal", order = 0)]
+    public class BoolGoal : Goal
+    {
+        [SerializeField] private BoolVariable variable;
+        [SerializeField] private bool requiredValue = true;
+
+        public override void Start()
+        {
+            base.Start();
+            variable.OnChange += Check;
+            Check(variable);
+        }
+
+        public override void Stop()
+        {
+            base.Stop();
+            variable.OnChange -= Check;
+        }
+
+        private void Check(bool newValue)
+        {
+            if (newValue == requiredValue)
+            {
+                onCompleted?.Invoke();
+            }
+        }
+    }
+}
