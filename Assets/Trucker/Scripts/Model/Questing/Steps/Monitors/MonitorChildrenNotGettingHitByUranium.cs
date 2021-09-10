7d2fc3e7538c9d2de4a59cf0ddc259e7540de4ad
Kirill Smirnov <thegaldren@gmail.com>
2021-09-10 15:13:41 +0300

    simplify MonitorChildrenNotGettingHitByUranium.cs

--- Assets/Trucker/Scripts/Model/Questing/Steps/Monitors/MonitorChildrenNotGettingHitByUranium.cs
+++ Assets/Trucker/Scripts/Model/Questing/Steps/Monitors/MonitorChildrenNotGettingHitByUranium.cs
@@ -1,13 +1,13 @@
 ﻿using Trucker.Control.Entities;
-using Trucker.Model.Zap.Level;
 using UnityEngine;
+using UnityUtils.Variables;
 
 namespace Trucker.Model.Questing.Steps.Monitors
 {
     [CreateAssetMenu(menuName = "Quests/Monitors/MonitorChildrenNotGettingHitByUranium", fileName = "MonitorChildrenNotGettingHitByUranium", order = 0)]
     public class MonitorChildrenNotGettingHitByUranium : Monitor
     {
-        [SerializeField] private ZapLevelVariable zapLevelVariable;
+        [SerializeField] private BoolVariable zapProtectionEnabled;
         
         public override void Start()
         {
@@ -19,7 +19,7 @@ namespace Trucker.Model.Questing.Steps.Monitors
             UraniumHitWatcher.OnUraniumHit -= FailQuest;
         }
 
-        private bool HasZapProtection => zapLevelVariable.Value == ZapLevel.Plus;
+        private bool HasZapProtection => zapProtectionEnabled.Value;
 
         private void FailQuest()
         {
