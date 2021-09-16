40daff6c882e5a71da5837b808abb4f373331eed
Kirill Smirnov <thegaldren@gmail.com>
2021-09-16 18:10:04 +0300

    Oneliners.cs

---
+++ Assets/Trucker/Scripts/Model/Dialogues/Oneliners.cs
@@ -0,0 +1,45 @@
+﻿using Trucker.Model.NPC;
+using UnityEngine;
+
+namespace Trucker.Model.Dialogues
+{
+    public class Oneliners : ScriptableObject
+    {
+        [SerializeField] private DialogueLine[] lines;
+
+        private IDialogue[] _entries;
+        
+        public IDialogue[] Entries
+        {
+            get
+            {
+                if (_entries == null) SetEntries();
+                return _entries;
+            }
+        }
+
+        private void OnValidate() => SetEntries();
+
+        private void SetEntries()
+        {
+            _entries = new IDialogue[lines.Length];
+            for (var i = 0; i < lines.Length; i++)
+            {
+                var line = lines[i];
+                _entries[i] = new OneLineDialogue {dialogueLine = line};
+            }
+        }
+
+        private struct OneLineDialogue : IDialogue
+        {
+            public DialogueLine dialogueLine;
+
+            public CharacterName InitialCharacter => dialogueLine.character;
+            public DialogueLine[] Lines => new[] {dialogueLine};
+            public string FirstLine => dialogueLine.line;
+            public void OnDialogueEnd() { }
+            public bool AvailableAsDialogueOption() => true;
+        }
+
+    }
+}
