9ceb25969a6900d4aea3417e61251ab5fe0704dd
Kirill Smirnov <thegaldren@gmail.com>
2021-09-17 15:10:40 +0300

    ParallelGoal.cs and Goal_Q11_RecordAllSongs.asset

---
+++ Assets/Trucker/Scripts/Model/Questing/Steps/Goals/ParallelGoal.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace Trucker.Model.Questing.Steps.Goals
+{
+    [CreateAssetMenu(menuName = "Quests/Goals/ParallelGoal", fileName = "ParallelGoal", order = 0)]
+    public class ParallelGoal : Goal
+    {
+        [SerializeField] private Goal[] goals;
+        
+        public override void Start()
+        {
+            base.Start();
+            foreach (var goal in goals)
+            {
+                goal.onCompleted += OnSubGoalCompleted;
+                goal.Start();
+            }
+        }
+
+        public override void Stop()
+        {
+            base.Stop();
+            foreach (var goal in goals)
+            {
+                goal.onCompleted -= OnSubGoalCompleted;
+                goal.Stop();
+            }
+        }
+
+        private void OnSubGoalCompleted()
+        {
+            Completed = goals.All(goal => goal.Completed);
+        }
+    }
+}
