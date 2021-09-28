9af8d93e031e5afa281f37ecb8d2944a245cdf10
Kirill Smirnov <thegaldren@gmail.com>
2021-09-28 12:32:45 +0300

    SpawnPrefab.cs operation

---
+++ Assets/Trucker/Scripts/Model/Questing/Steps/Operations/SpawnPrefab.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Trucker.Model.Questing.Steps.Operations
+{
+    [CreateAssetMenu(menuName = "Quests/Operations/SpawnPrefab", fileName = "SpawnPrefab", order = 0)]
+    public class SpawnPrefab : Operation
+    {
+        [SerializeField] private GameObject prefab;
+        
+        public override void Start()
+        {
+            Instantiate(prefab);
+            onCompleted?.Invoke();
+        }
+    }
+}
