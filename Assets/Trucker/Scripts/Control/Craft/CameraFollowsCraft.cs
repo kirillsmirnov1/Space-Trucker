1469d7cf9be29d10fdc20de6f3c3651c1ca31450
Kirill Smirnov <thegaldren@gmail.com>
2021-09-30 13:02:01 +0300

    Fix CameraFollowsCraft.cs lunge on 90 degrees rotation

--- Assets/Trucker/Scripts/Control/Craft/CameraFollowsCraft.cs
+++ Assets/Trucker/Scripts/Control/Craft/CameraFollowsCraft.cs
@@ -2,17 +2,14 @@
 using Trucker.Model.Craft;
 using UnityEngine;
 using UnityUtils;
-using UnityUtils.Extensions;
 
 namespace Trucker.Control.Craft
 {
     public class CameraFollowsCraft : MonoBehaviour
     {
         [SerializeField] private Transform cameraPivot;
-        [SerializeField] private Transform craft;
         [SerializeField] private CameraFollowsCraftSettings settings;
         [SerializeField] private ShipModelParamsVariable shipModelParams;
-        
         
         private void OnValidate() => this.CheckNullFieldsIfNotPrefab();
 
@@ -30,11 +27,8 @@ namespace Trucker.Control.Craft
         
         private void MoveRotation()
         {
-            var pointToLookAt = craft.position + craft.forward * settings.craftForwardLookDistance;
-            var targetRotation = Quaternion.LookRotation(pointToLookAt - transform.position);
-            if(craft.UpsideDown()) targetRotation *= Quaternion.Euler(0, 0, 180); // FIXME should change smoothly 
             var slerpedRotation =
-                Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * settings.speedRotate); 
+                Quaternion.Slerp(transform.rotation, cameraPivot.rotation, Time.deltaTime * settings.speedRotate); 
             transform.rotation = slerpedRotation;
         }
     }
